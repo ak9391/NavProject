e8171285655b0f471379c3fa3ac4dbf7046f80b9
ak9391 <ak9391@nyu.edu>
2023-07-14 20:59:14 +0530

    Initial Commit

---
+++ NavProject/NavProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavProjectApp.swift
+//  NavProject
+//
+//  Created by Ashrita Kantamneni on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NavProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
